ab6d0950bc459059a9ab69edf42a8f0de9400e87
Documenter.jl <documenter@juliadocs.github.io>
2025-06-24 21:46:28 +0000

    build based on 498d9de

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PhasedArrays","page":"Home","title":"PhasedArrays","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PhasedArrays.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
